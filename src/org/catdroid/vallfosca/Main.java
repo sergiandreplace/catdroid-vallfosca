eb22fb88509cf1d36928fa13231249b8e361daa4
jordi.bernabeu <jordi.bernabeu@localhost>
2011-06-04 17:57:05 +0200

    integration with mixare

--- src/org/catdroid/vallfosca/Main.java
+++ src/org/catdroid/vallfosca/Main.java
@@ -1,6 +1,8 @@
 package org.catdroid.vallfosca;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 public class Main extends Activity {
@@ -9,6 +11,10 @@ public class Main extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
+
+        Intent i = new Intent();
+        i.setAction(Intent.ACTION_VIEW);      
+        i.setDataAndType(Uri.parse("file://sdcard/vallfosca/json_demo.txt"), "application/mixare-json");
+        startActivity(i);
     }
 }
