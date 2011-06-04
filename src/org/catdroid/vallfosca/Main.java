83dc1be4d93b59a95fc2b137ab8cfd9a910e64d2
sergi.martinez <sergi.martinez@localhost>
2011-06-04 17:32:29 +0200

    menu created

--- src/org/catdroid/vallfosca/Main.java
+++ src/org/catdroid/vallfosca/Main.java
@@ -2,12 +2,43 @@ package org.catdroid.vallfosca;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class Main extends Activity {
+public class Main extends Activity implements OnClickListener {
+	LanguageManager languageManager;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        languageManager=new LanguageManager(this);
+        
+        findViewById(R.id.option_info).setOnClickListener(this);
+        findViewById(R.id.option_routes).setOnClickListener(this);
+       
     }
+	@Override
+	public void onClick(View v)
+	{
+		switch (v.getId())
+		{
+			case R.id.option_info:
+				View group_info = findViewById(R.id.group_info);
+				if (group_info.getVisibility()==View.VISIBLE) 
+					group_info.setVisibility(View.GONE);
+				else 
+					group_info.setVisibility(View.VISIBLE);
+				break;
+			case R.id.option_routes:
+				View group_routes = findViewById(R.id.group_routes);
+				if (group_routes.getVisibility()==View.VISIBLE) 
+					group_routes.setVisibility(View.GONE);
+				else 
+					group_routes.setVisibility(View.VISIBLE);
+				break;
+			
+		}
+		
+	}
 }
