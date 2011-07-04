6cc61700b0395a8d24da19212f610f6ff885578f
jordi bernabeu <jordi.bernabeu@gmail.com>
2011-07-05 01:38:54 +0200

    Included POI file for Barcelona. BCN POIs loaded when clicking on the "Punts d'inter?s" button in main menu.

--- src/org/catdroid/vallfosca/Main.java
+++ src/org/catdroid/vallfosca/Main.java
@@ -16,24 +16,32 @@ public class Main extends Activity implements OnClickListener {
         setContentView(R.layout.main);
  
         findViewById(R.id.option_villages).setOnClickListener(this);
-//        findViewById(R.id.option_routes).setOnClickListener(this);
+        findViewById(R.id.option_pois).setOnClickListener(this);
 //        findViewById(R.id.option_virtual).setOnClickListener(this);
                
 //        languageManager=new LanguageManager(this);
         FileManager.saveas(R.raw.vallfosca, "//sdcard/vallfosca/", "vallfosca.json", this);
+        FileManager.saveas(R.raw.barcelona, "//sdcard/vallfosca/", "barcelona.json", this);
        
     }
 	
 	public void onClick(View v)
 	{
+        Intent i = new Intent();
 		switch (v.getId())
 		{
 			
 			case R.id.option_villages:
-		        Intent i = new Intent();
 		        i.setAction(Intent.ACTION_VIEW);      
-		       
 		        i.setDataAndType(Uri.parse("file://sdcard/vallfosca/vallfosca.json"), "application/mixare-vallfosca-json");
+//		        i.setDataAndType(Uri.parse("org.catdroid.vallfosca:raw/vallfosca"), "application/mixare-json");
+		        startActivity(i);
+
+		        break;
+
+			case R.id.option_pois:
+		        i.setAction(Intent.ACTION_VIEW);      
+		        i.setDataAndType(Uri.parse("file://sdcard/vallfosca/barcelona.json"), "application/mixare-vallfosca-json");
 //		        i.setDataAndType(Uri.parse("org.catdroid.vallfosca:raw/vallfosca"), "application/mixare-json");
 		        startActivity(i);
 
